f060a950f143e5aeb1ab6f4243b2f7f739d50696
Peter Aiello <peteraiello@Peters-Mac-mini.fritz.box>
2020-11-06 16:39:42 +0000

    canonical link all good to go

--- packages/peter-aiello-theme/src/components/Post.js
+++ packages/peter-aiello-theme/src/components/Post.js
@@ -20,6 +20,11 @@ const Post = ({ state }) => {
     /* post id */
     const date = new Date(post.date);
 
+    /* get the head tags object */
+    const headTags = state.headTags.get(post.link);
+    /* get the canonical link */
+    const canonicalRef = headTags[6].attributes.content;
+
     const BackgroundImage = styled.div`
         min-height: 100vh;
         height: 1250px;
@@ -42,6 +47,7 @@ const Post = ({ state }) => {
 
             <Head>
                 <title>{ post.title.rendered }</title>
+                <link rel="canonical" href={canonicalRef} />
             </Head>
 
             <BackgroundImage>
