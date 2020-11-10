8b65eb32f72ced19ea79b12a5aab9ef3d85512c5
Peter Aiello <peteraiello@Peters-Mac-mini.fritz.box>
2020-11-10 12:50:11 +0000

    add menu animation

--- packages/peter-aiello-theme/src/components/Menu/Nav.js
+++ packages/peter-aiello-theme/src/components/Menu/Nav.js
@@ -1,25 +1,53 @@
 import React from "react";
 import Link from "@frontity/components/link";
-import { connect, styled } from "frontity";
+import { connect, styled, keyframes, css} from "frontity";
 
 const Nav = ({ state, actions }) => (
     <>
-      {state.theme.menu.map(([name, link]) => {
+      {state.theme.menu.map(([name, link], index) => {
         // Check if the link matched the current page url
         const isCurrentPage = state.router.link === link;
+
+        // add a delay
+        const Stagger = 100;
+        const fadeIn = keyframes`
+          from {
+            opacity: 0;
+            display: none;
+          }
+          to {
+            opacity: 1;
+            display: block;
+          }
+        `
         return (
-          <div key={name}>
+          <div key={index}>
             {
               // If link url is the current page, add `aria-current`.
               // also, use an onClick function to close the menu.   
             }
-            <Link key={name} onClick={() => actions.theme.closeMenu()} link={link} aria-current={isCurrentPage ? "page" : undefined}>
+            <NavItem 
+              css={css`
+                  animation: ${fadeIn};
+                  animation-duration: 1s;
+                  animation-timing-function: linear;
+                  animation-delay: ${Stagger * index }ms;
+                  animation-repeat: no-repeat;
+                  animation-fill-mode: both;
+                `
+              }
+              key={name} onClick={() => actions.theme.closeMenu()} link={link} aria-current={isCurrentPage ? "page" : undefined}>
               {name}
-            </Link>
+            </NavItem>
           </div>
         );
       })}
     </>
   );
+
+
+    
+const NavItem = styled(Link)`
+`
 
 export default connect(Nav);
