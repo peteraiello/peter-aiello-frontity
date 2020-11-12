159e6ab92f2a08d70dea00c79ee14cbba5555887
Peter Aiello <peteraiello@Peters-Mac-mini.fritz.box>
2020-11-12 13:52:01 +0000

    better sticky menu

--- packages/peter-aiello-theme/src/components/Header.js
+++ packages/peter-aiello-theme/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect, keyframes } from "frontity";
+import { connect, keyframes, Global, css } from "frontity";
 import MainMenu from "./Menu/MainMenu";
 import StickyMenu from "./Menu/StickyMenu";
 import { styled } from "frontity";
@@ -12,6 +12,29 @@ const Header = ({ state, actions }) => {
         // menu not in view?  update 'menuIsHidden'
         // state to true
 
+
+        const FadeOut = keyframes`
+        0% {
+            display: block;
+            opacity: 1; 
+        }
+        100% {
+            display: none;
+            opacity: 0;  
+        }
+        `;
+
+        const FadeIn = keyframes`
+            0% {
+                display: none;
+                opacity: 0; 
+            }
+            100% {
+                display: block;
+                opacity: 1;  
+            }
+        `;
+
         const [ref, inView] = useInView({threshold: 0,});
 
         inView ? ( 
@@ -22,45 +45,42 @@ const Header = ({ state, actions }) => {
 
         return(
             <>
-                    <MenuDefault ref={ref}>
-                        <MainMenu logoSize="120px" />
-                    </MenuDefault>
-                {
-                    !state.theme.menuIsHidden &&
-                        <MenuScrollOut>
-                            <StickyMenu logoSize="80px" />
-                        </MenuScrollOut>
-                }
                 {
                     state.theme.menuIsHidden &&
-                        <MenuScrollIn>
-                            <StickyMenu logoSize="80px" />
-                        </MenuScrollIn>
+                    <Global styles={css`
+                    .sticky-menu {
+                        position: fixed;
+                        top: 0;
+                        z-index: 999;
+                        width: 100%;
+                        animation: ${FadeIn};
+                        animation-duration: 1s;
+                        background: var(--dark-grey);
+                        z-index: 9999;
+                    }}
+                `} />
                 }
+                {
+                    !state.theme.menuIsHidden &&
+                    <Global styles={css`
+                    .sticky-menu {
+                        background: var(--dark-grey);
+                        animation: ${FadeOut};
+                        animation-duration: 1s;
+                        animation-fill-mode: both;
+                    }}
+                ` } />
+                }
+                <MenuDefault ref={ref}>
+                    <MainMenu logoSize="120px" />
+                </MenuDefault>
+                <div class="sticky-menu">
+                    <StickyMenu logoSize="80px" />
+                </div>
             </>
         )
 }
-const FadeOut = keyframes`
-    from {
-        display: block;
-        opacity: 1; 
-    }
-    to {
-        opacity: 0;  
-        display: none;
-    }
-`;
 
-const FadeIn = keyframes`
-    0% {
-        display: none;
-        opacity: 0; 
-    }
-    100% {
-        display: block;
-        opacity: 1;  
-    }
-`;
 
 const MenuDefault = styled.div`
     background: #00000080; 
@@ -75,36 +95,5 @@ const MenuDefault = styled.div`
     }
 `
 
-const MenuScrollOut = styled.div`
-    display: none;
-    background: #00000080; 
-    position: fixed; 
-    width: 100%; 
-    top: 0; 
-    z-index: 9998;
-    animation: ${FadeOut};
-    animation-duration: 500ms;
-    animation-fill-mode: both;
-    box-sizing: border-box;
-    @media (min-width: 1024px){
-        display: block;
-    }
-`
-
-const MenuScrollIn = styled.div`
-    display: none;
-    background: #00000080; 
-    position: fixed; 
-    width: 100%; 
-    top: 0; 
-    z-index: 9999;
-    animation: ${FadeIn};
-    animation-duration: 1s;
-    animation-fill-mode: both;
-    box-sizing: border-box;
-    @media (min-width: 1024px){
-        display: block;
-    }
-`
 
 export default connect(Header);
