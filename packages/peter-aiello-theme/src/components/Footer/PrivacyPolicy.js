ce9d389352f78dbdd5d5103b2d6d6ce7f5f0900e
Peter Aiello <peteraiello@Peters-Mac-mini.fritz.box>
2020-11-25 18:39:14 +0000

    remove modal script from privacy link

--- packages/peter-aiello-theme/src/components/Footer/PrivacyPolicy.js
+++ packages/peter-aiello-theme/src/components/Footer/PrivacyPolicy.js
@@ -4,7 +4,7 @@ import {connect} from "frontity";
 
 const PrivacyPolicy = ({ libraries }) => {
     const Html2React = libraries.html2react.Component;
-    const privacyHtml = '<a href="https://www.iubenda.com/privacy-policy/74388008" class="iubenda-white iubenda-embed" title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>';
+    const privacyHtml = '<a href="https://www.iubenda.com/privacy-policy/74388008" target="_blank" title="Privacy Policy ">Privacy Policy</a>';
     return (
         <Html2React html={ privacyHtml } />
     )
